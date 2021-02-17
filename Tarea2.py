99678df3a6c550da77b8f90ce232a0874a5b0a5b
pamejinezv <78750560+pamejinezv@users.noreply.github.com>
2021-02-17 16:20:39 -0600

    Tarea 2
    Tarea 2

---
+++ Tarea2.py
@@ -0,0 +1,44 @@
+import re
+
+
+correo = 'pame.jinezv@gmail.com.mx'
+patron = '^[(a-zA-Z0-9\_\-\.)]+@[(a-zA-Z\_\-\.)]+\.[(a-zA-Z)]{2,}$'
+valida = re.match(patron, correo)
+
+if valida:
+   print('El correo electronico Correcto')
+else:
+    print('Correo electronico Incorrecto')
+
+
+numeroTelefonico = '(188)1456719'
+patronN= '^\(([1-9]{3,})\)[0-9]{7}$'
+patronSP = '^[1-9]{1}[0-9]{9,}$'
+
+validaNCP = re.match(patronN, numeroTelefonico)
+validaSP = re.match(patronSP, numeroTelefonico)
+if validaNCP or validaSP:
+   print('El numero telefonico Correcto')
+else:
+    print('El numero telefonico Incorrecto')
+
+
+curp = 'JIVP910813MGTNLM05'
+patronC = '^[a-zA-Z]{4}[0-9]{6}[HM]{1}[a-zA-Z]{5}[0-9]{2}$'
+validarCurp = re.match(patronC, curp)
+
+if validarCurp:
+    print('La CURP es Correcta')
+else:
+    print('La CURP es Incorrecta')
+
+
+rfc = 'JIVP910813797'
+patronR = '^[a-zA-Z]{4}[0-9]{6}[0-9]{3}$'
+validarRfc = re.match(patronR, rfc)
+
+if validarRfc:
+    print('El RFC es Correcto')
+else:
+    print('El RFC es Incorrecto')
+
